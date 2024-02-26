446147d7cc10a6f42b23018fe36e3c3d06af8c71
unknown <chlghksdyd31@gmail.com>
2024-02-26 17:15:08 +0900

    2024-02-26

--- untitled/src/main/java/jpabook/jpashop/JpaMain.java
+++ untitled/src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
@@ -19,15 +20,28 @@ public class JpaMain {
         tx.begin();
 
         try{
+            Member member1 = new Member();
+            member1.setName("member1");
+            em.persist(member1);
 
-            Member member = new Member();
-            member.setName("Kanye");
-            em.persist(member);
+            em.flush();
+            em.clear();
+
+            Member refMember = em.getReference(Member.class, member1.getId());
+
+            System.out.println("refMember = "+refMember.getClass());
+
+            System.out.println("isLoaded = "+emf.getPersistenceUnitUtil().isLoaded(refMember));
+
+            refMember.getName();
+
+            System.out.println("isLoaded = "+emf.getPersistenceUnitUtil().isLoaded(refMember));
 
             tx.commit();
 
         }catch (Exception e){
             tx.rollback();
+            e.printStackTrace();
         }finally {
             em.close();
         }
